c64a15421b5e74e2fa313333f302c2b82795fa9c
JoaoScheid <142051698+JoaoScheid@users.noreply.github.com>
2023-10-10 21:54:14 -0300

    aaaa
    aaaa

--- src/Atv4Matriz.java
+++ src/Atv4Matriz.java
@@ -14,19 +14,23 @@ public class Atv4Matriz {
 			for(int coluna=0;coluna<6;coluna++) {
 			System.out.println("Digite o valor da posição ["+linha+"]["+coluna+"]");
 			matriz[linha][coluna]= sc.nextInt();
+			if(matriz[linha][coluna]==num) {
+				System.out.println("O valor esta na linha ["+ linha+"] e na coluna [" +coluna+"]");
 			}
-		}
-		for(int linha=0;linha<6;linha++) {
-			for(int coluna=0;coluna<6;coluna++)
-		if(matriz[linha][coluna]==num) {
-			System.out.println("O valor esta na linha ["+ linha+"] e na coluna [" +coluna+"]");
-		}
-		else {
-			System.out.println("Valor não encontrado");
-		}
+			else {
+				System.out.println("Valor não encontrado");
+			}
+			}
 		}
 	}
 }
+			
+		
+	
+	
+		
+	
+
 		
 	
 
--- src/Atv5Matriz.java
+++ src/Atv5Matriz.java
@@ -8,6 +8,17 @@ public class Atv5Matriz {
 		
 		int matriz[][]= new int[4][4];
 		
+		for(int linha=0;linha<4;linha++) {
+			for(int coluna=0;coluna<4;coluna++) {
+				System.out.println(" Digite o valor da posição["+linha+"]["+coluna+"]");
+				matriz[linha][coluna]= sc.nextInt();
+			}
 	}
+		System.out.println("Diagonais: ");
+		System.out.println(matriz[0][0]);
+		for(int linha=0;linha<4;linha++) {
+			
+		}
 
+}
 }
